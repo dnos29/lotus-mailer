01d997f7844c36b89049f510610bdb51a2b7af8f
Sotatek-MattDang <son.dangngoc@setel.com>
2024-08-14 10:12:30 +0700

    init

---
+++ pages/api/review-reminder.ts
@@ -0,0 +1,13 @@
+import { NextApiRequest, NextApiResponse } from "next"
+
+type ResponseData = {
+  message: string
+}
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseData>
+) {
+  res.status(200).json({ message: 'Hello from Next.js!' })
+}
+//lifiZqFZj5GoyvLW
