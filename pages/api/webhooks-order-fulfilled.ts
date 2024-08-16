d815e11d562ada230a7cd86da8324d3c87ab8216
Sotatek-MattDang <son.dangngoc@setel.com>
2024-08-16 16:17:46 +0700

    fix create order

--- pages/api/webhooks-order-fulfilled.ts
+++ pages/api/webhooks-order-fulfilled.ts
@@ -12,9 +12,10 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseData>
 ) {
-  return await supabase.from('orders').insert({
+  await supabase.from('orders').insert({
     order_id: req.body?.id,
-    raw_request: JSON.stringify(req.body),
+    raw_request: JSON.stringify(req?.body),
     due_date: dayjs().weekday(3),
   });
+  res.status(200).json({ message: 'Order created' })
 }
